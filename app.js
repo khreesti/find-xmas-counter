dd8a19947ffc6e7d3f0adae83277f4363fe49b1a
Kristy Ong <kristy.mil.ong@gmail.com>
2018-11-28 17:18:51 -0700

    starter code

---
+++ app.js
@@ -0,0 +1,9 @@
+// Coundown to Dec 25
+
+// html element grabber
+// get the date for today
+// get the date that we want (Dec 25)
+// calculation, to figure out the difference in the time
+// display on the html
+
+// eventlistener, onchange, the change of the difference calculation
